36479298fb5b6e210ef0a9ec4fefe23bd9503563
ciacci <f.debitonto@yahoo.com>
2017-07-04 16:11:01 +0200

    heroku changes 2

--- bot.py
+++ bot.py
@@ -9,16 +9,16 @@ def handle(message):
     if content_type == "text":
         msg=message['text']
         if msg== "/sabino":
-            bot.sendPhoto(chat_id,open("/home/francesco/Telegram/img/sabino.jpg","rb"))
+            bot.sendPhoto(chat_id,open("/home/francesco/bot/chat-bot/Telegram/img/sabino.jpg","rb"))
         elif msg== "/chat":
-            img_path="/home/francesco/Telegram/img/chat/"
+            img_path="/home/francesco/bot/chat-bot/Telegram/img/chat/"
             img_name=random.choice(os.listdir(img_path))
             img=open(img_path+img_name,"rb")
             bot.sendPhoto(chat_id,img)
             bot.sendMessage(chat_id,"Awend ne")
         elif msg =="/spaghitt":
-            bot.sendPhoto(chat_id,open("/home/francesco/Telegram/img/spaghitt.jpg","rb"))
-            bot.sendAudio(chat_id,open("/home/francesco/Telegram/audio/spaghitt.ogg","rb"))
+            bot.sendPhoto(chat_id,open("/home/francesco/bot/chat-bot/Telegram/img/spaghitt.jpg","rb"))
+            bot.sendAudio(chat_id,open("/home/francesco/bot/chat-bot/Telegram/audio/spaghitt.ogg","rb"))
             bot.sendMessage(chat_id,"300 gremm d spaghitt, ma vou seit pezz!")
         else:
             if any(x in msg for x in ["nicola","Nicola","ncol","Ncol","nco","Nco","Violo","violo","Violante","violante"]):
